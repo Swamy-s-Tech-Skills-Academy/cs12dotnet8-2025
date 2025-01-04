41ee5bf2cf528923389712ebc6f9c6a1cd81fd92
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-01-04 18:32:26 +0530

    Code Improvements

--- src/Ch02/Arguments/Program.cs
+++ src/Ch02/Arguments/Program.cs
@@ -1,5 +1,18 @@
-﻿ResetColor();
-ForegroundColor = ConsoleColor.DarkCyan;
+﻿ForegroundColor = ConsoleColor.DarkCyan;
+
+if (args.Length != 3)
+{
+    ForegroundColor = ConsoleColor.Red;
+
+    WriteLine("\nUsage: dotnet run <foreground_color> <background_color> <cursor_size>");
+    WriteLine("Example: dotnet run Red Yellow 50");
+    WriteLine("Available colors: Black, DarkBlue, DarkGreen, DarkCyan, DarkRed, DarkMagenta, DarkYellow, Gray, DarkGray, Blue, Green, Cyan, Red, Magenta, Yellow, White");
+    
+    ResetColor();
+
+    return;
+}
+
 WriteLine($"There are {args.Length} arguments.");
 
 foreach (string arg in args)
@@ -7,34 +20,24 @@ foreach (string arg in args)
     Write($"{arg} :: ");
 }
 
-if (args.Length != 3)
-{
-    WriteLine("\nUsage: dotnet run <foreground_color> <background_color> <cursor_size>");
-    WriteLine("Example: dotnet run Red Yellow 50");
-    WriteLine("Available colors: Black, DarkBlue, DarkGreen, DarkCyan, DarkRed, DarkMagenta, DarkYellow, Gray, DarkGray, Blue, Green, Cyan, Red, Magenta, Yellow, White");
-    ResetColor();
-
-    return;
-}
-
 if (!Enum.TryParse(args[0], true, out ConsoleColor foregroundColor))
 {
     WriteLine($"Invalid foreground color: {args[0]}");
-    
+
     return;
 }
 
 if (!Enum.TryParse(args[1], true, out ConsoleColor backgroundColor))
 {
     WriteLine($"Invalid background color: {args[1]}");
-    
+
     return;
 }
 
 if (!int.TryParse(args[2], out int cursorSize) || cursorSize < 1 || cursorSize > 100)
 {
     WriteLine($"Invalid cursor size. Must be a number between 1 and 100: {args[2]}");
-    
+
     return;
 }
 
