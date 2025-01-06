1d2a9e119767ec325f90e333ad74e44bdf76637f
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-01-06 13:35:48 +0530

    Update Program.cs

--- src/Ch01/HelloCS/Program.cs
+++ src/Ch01/HelloCS/Program.cs
@@ -2,15 +2,18 @@
 
 WriteLine("Hello, C# 12, and DotNet 8 !");
 
-WriteLine("We received {0} arguments:", args.Length);
-foreach (string arg in args)
+WriteLine($"We received {args?.Length ?? 0} arguments:");
+if (args != null)
 {
-    WriteLine(arg);
+    foreach (string arg in args)
+    {
+        WriteLine(arg);
+    }
 }
 
-WriteLine("\nCurrentDirectory: {0}", Env.CurrentDirectory);
-WriteLine("OSVersion.VersionString: {0}", Env.OSVersion.VersionString);
-WriteLine("Namespace: {0}", typeof(Program).Namespace ?? "None!");
+WriteLine(Environment.NewLine + $"CurrentDirectory: {Env.CurrentDirectory}");
+WriteLine($"OSVersion.VersionString: {Env.OSVersion.VersionString}");
+WriteLine($"Namespace: {typeof(Program).Namespace ?? "None!"}");
 
 ResetColor();
 
